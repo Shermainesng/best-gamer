dc707a9441329bec6c463036676e4e103d96fae9
Huiling Loh <huiling_loh_97@hotmail.com>
2021-11-29 17:36:50 +0800

    migrate coach models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_091625) do
+ActiveRecord::Schema.define(version: 2021_11_29_093444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coach_games", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_coach_games_on_game_id"
+    t.index ["user_id"], name: "index_coach_games_on_user_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,4 +53,6 @@ ActiveRecord::Schema.define(version: 2021_11_29_091625) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "coach_games", "games"
+  add_foreign_key "coach_games", "users"
 end
