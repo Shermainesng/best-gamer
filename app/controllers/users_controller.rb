ab7efea34f15115c08899ad2bc986da5c973e1c5
jiaen zhang <jiaen.zhang98@gmail.com>
2021-11-30 13:44:15 +0800

    fixed routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,12 @@
 class UsersController < ApplicationController
-  def filter_by_game
-    @game = Game.find(game_params[:id])
+  def index
+    @game = Game.find_by(name: params[:game])
     @users = @game.users
   end
 
   private
 
-  def game_params
-    params.require(:game).permit(:id)
-  end
+
 
   # def filter_games_params
   #   params.require(:)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
   resources :orders, only: [:new, :create]
-  post 'coaches/:id', to: 'users#filter_by_game'
+
+  get 'coaches/:game', to: 'users#index'
+
   # get 'coaches/:id/', to: 'users#filter'
   # get 'coaches/:id', to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
