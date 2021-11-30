dae27305bfee16b9a6a3741480fd24572b89e217
jiaen zhang <jiaen.zhang98@gmail.com>
2021-11-30 12:28:41 +0800

    M

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   def filter_by_game
-
     @game = Game.find(game_params[:id])
     @users = @game.users
-    # redirect_to 'co'
+  end
+
+  def show
+    @user = User.find(params["id"])
   end
 
   private
+
   def game_params
     params.require(:game).permit(:id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :orders, only: [:new, :create]
   post 'coaches/:id', to: 'users#filter_by_game'
-  # get 'coaches/:id', to: 'users#index'
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
