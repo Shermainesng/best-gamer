3cfe1d09b9dd04128a181b94746663df75dddbe0
Huiling Loh <huiling_loh_97@hotmail.com>
2021-11-30 12:59:32 +0800

    added filter form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
   def filter_by_game
-
     @game = Game.find(game_params[:id])
     @users = @game.users
-    # redirect_to 'co'
   end
 
   private
+
   def game_params
     params.require(:game).permit(:id)
   end
+
+  # def filter_games_params
+  #   params.require(:)
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   post 'coaches/:id', to: 'users#filter_by_game'
+  # get 'coaches/:id/', to: 'users#filter'
   # get 'coaches/:id', to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
