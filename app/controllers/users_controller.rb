e50d508ee5137784f64391dcb47166dfacfae8a7
jiaen zhang <jiaen.zhang98@gmail.com>
2021-12-03 16:31:07 +0800

    remove card deck + review logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,11 @@ class UsersController < ApplicationController
     @coach_slots = Slot.by_user(@user)
     @available_slots = @coach_slots.available
     @available_dates = @available_slots.map { |slot| slot.date }
+    @reviews_count = @user.reviews.count.to_f
 
-    unless @user.reviews.nil?
+    unless @reviews_count.zero?
+      @avg_rating = @user.reviews.average("rating").to_i
       @reviews = @user.reviews
-      @reviews_count = @reviews.count.to_f
-      @avg_rating = @reviews.average("rating").to_i
     end
     @booked_dates = get_booked_dates(current_user) if user_signed_in?
   end
