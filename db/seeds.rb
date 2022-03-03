df7384d79be802096a0fd74b91f387618c2bfe63
jiaen zhang <jiaen.zhang98@gmail.com>
2022-03-03 14:09:05 +0800

    clear Orders from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-puts 'Creating games'
-
+puts "clearing seeds"
+Order.destroy_all
 Slot.destroy_all
 Game.destroy_all
 User.destroy_all
+puts 'Creating games'
+
 
 games = ['League of Legends', 'Valorant', "Dota 2", "Fortnite"]
 games.each do |game|
